912c6770efa32f0b38bc16155d9ea354bdda0a5e
Jim Bowring <bowring@gmail.com>
2022-02-07 12:44:23 -0500

    Updated headers

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/TripoliAboutWindow.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/TripoliAboutWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 James F. Bowring and CIRDLES.org.
+ * Copyright 2022 James Bowring, Noah McLean, Scott Burdick, and CIRDLES.org.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.cirdles.tripoli.gui;
 
 import javafx.event.EventHandler;
