58cbe04cdc12b75a7e98242709c099b4bb00dc8b
Jim Bowring <bowring@gmail.com>
2023-04-09 08:19:52 -0400

    Tweaked stage behavior.

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/plotsControllers/mcmcPlots/MCMCPlotsWindow.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/plotsControllers/mcmcPlots/MCMCPlotsWindow.java
@@ -59,6 +59,10 @@ public class MCMCPlotsWindow {
         MCMCPlotsController.analysisManagerCallbackI = analysisManagerCallbackI;
     }
 
+    public void close() {
+        plottingStage.close();
+    }
+
     public void loadPlotsWindow() {
         if (!plottingStage.isShowing()) {
             try {
