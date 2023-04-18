7e5a1e60067d80d03c3008927b56596d7ee3ca75
Ian Robinson <ianrrob2@aol.com>
2023-04-18 00:45:08 -0400

    updated peak shapes plot title

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/plotsControllers/peakShapePlots/PeakShapeDemoPlotsControllerTest.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/plotsControllers/peakShapePlots/PeakShapeDemoPlotsControllerTest.java
@@ -455,7 +455,9 @@ public class PeakShapeDemoPlotsControllerTest {
                 PlotBuilder[] plots = BeamDataOutputDriverExperiment.modelTest(resourceBrowserTarget.toPath(), this::populateListOfResources);
                 PeakShapeOutputDataRecord peakShapeOutputDataRecord = BeamShapeLinePlotBuilderX.getPeakData(resourceBrowserTarget.toPath());
                 //BeamShapeLinePlotBuilderX beamShape = BeamShapeLinePlotBuilderX.initializeBeamShape(peakShapeOutputDataRecord, new String[]{peakShapeOutputDataRecord.massID()}, "Mass (amu)", "Beam Intensity");
-                PeakShapesOverlayBuilder peakShapes = PeakShapesOverlayBuilder.initializePeakShapes(peakShapeOutputDataRecord, new String[]{peakShapeOutputDataRecord.massID()}, "Mass (amu)", "Intensities");
+                PeakShapesOverlayBuilder peakShapes = PeakShapesOverlayBuilder.initializePeakShapes(peakShapeOutputDataRecord,
+                        new String[]{ "Peak Shapes " + peakShapeOutputDataRecord.massID() + " / Peak Mass: " + peakShapeOutputDataRecord.peakCenterMass() },
+                        "Mass (amu)", "Peak Intensities");
                 if (ensemblePlotsWallPane.getChildren().size() > 3) {
                     ensemblePlotsWallPane.getChildren().remove(2, ensemblePlotsWallPane.getChildren().size());
 
