3cfb7074361b693c7296d5f9f6ffa4c1c7fe7b79
Jim Bowring <bowring@gmail.com>
2023-06-29 09:57:55 -0400

    new version

--- buildSrc/src/main/kotlin/common-build.gradle.kts
+++ buildSrc/src/main/kotlin/common-build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 
 val mavenArtifactId = name
 val mavenGroupId = "org.cirdles"
-val mavenVersion = "0.2.1"
+val mavenVersion = "0.2.2"
 
 object Versions {
     const val junitVersion = "5.8.2"
