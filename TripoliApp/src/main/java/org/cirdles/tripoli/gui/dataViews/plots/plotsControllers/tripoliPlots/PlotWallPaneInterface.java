83409c8b4d649c3a637d717581846e6b6ca08f12
Jim Bowring <bowring@gmail.com>
2023-10-30 08:26:58 -0400

    plotwallinterface

---
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/plotsControllers/tripoliPlots/PlotWallPaneInterface.java
@@ -0,0 +1,9 @@
+package org.cirdles.tripoli.gui.dataViews.plots.plotsControllers.tripoliPlots;
+
+public interface PlotWallPaneInterface {
+    public double getToolBarHeight();
+
+    public int getToolBarCount();
+    public void stackPlots();
+
+}
