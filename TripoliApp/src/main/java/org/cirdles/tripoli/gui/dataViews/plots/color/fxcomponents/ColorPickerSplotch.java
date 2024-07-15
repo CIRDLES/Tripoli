789ff55d9bf636359f874fff6725e65a6373ddcb
Dan Johnson <jdjohnson8883@gmail.com>
2024-07-15 13:10:17 -0400

    committing progress

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/color/fxcomponents/ColorPickerSplotch.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/color/fxcomponents/ColorPickerSplotch.java
@@ -1,5 +1,7 @@
 package org.cirdles.tripoli.gui.dataViews.plots.color.fxcomponents;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.ColorPicker;
@@ -7,41 +9,41 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.text.Font;
 
-public class ColorPickerSplotch extends ColorPicker {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ColorPickerSplotch extends ColorPicker implements Initializable {
+
+
     private Label colorSplotch;
 
     public ColorPickerSplotch() {
         super();
-        valueProperty().addListener((observable, oldValue, newValue) -> {updateBackgroundColor();});
     }
 
     private void updateBackgroundColor() {
-        if (colorSplotch != null) {
             colorSplotch.setBackground(
                     new Background(
                             new BackgroundFill(
                                     getValue(),
                                     CornerRadii.EMPTY,
                                     Insets.EMPTY)));
-        }
     }
 
-    @Override
-    protected void layoutChildren() {
-        super.layoutChildren();
-        if(colorSplotch == null) {
-            for (Node child : getChildrenUnmodifiable()) {
-                if (child instanceof Label) {
-                    colorSplotch = (Label) child;
-                    colorSplotch.prefWidthProperty().bind(widthProperty());
-                    colorSplotch.prefHeightProperty().bind(heightProperty());
-                    updateBackgroundColor();
-                }
-            }
-        }
-    }
+
+
 
     public Label getColorSplotch() {return colorSplotch;}
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.setStyle("-fx-color-label-visible: false; -fx-color-picker-visible: false;");
+        this.colorSplotch = new Label();
+        this.getChildren().add(colorSplotch);
+        colorSplotch.prefWidthProperty().bind(this.widthProperty());
+        colorSplotch.prefHeightProperty().bind(this.heightProperty());;
+        valueProperty().addListener((observable, oldValue, newValue) -> {updateBackgroundColor();});
+    }
 }
--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/color/fxcomponents/ColorPickerSplotchController.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/dataViews/plots/color/fxcomponents/ColorPickerSplotchController.java
@@ -1,16 +1,39 @@
 package org.cirdles.tripoli.gui.dataViews.plots.color.fxcomponents;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
 
 public class ColorPickerSplotchController {
 
+    @FXML
+    private ColorPicker colorPicker;
 
     @FXML
-    private ColorPickerSplotch colorPicker;
+    private Label label;
 
+    @FXML
+    private StackPane stackPane;
 
     @FXML
     private void initialize() {
+        // Bind the label's background color to the color picker's value
+        colorPicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            colorPicker.backgroundProperty().
+                    setValue(
+                            new Background(
+                                    new BackgroundFill(newValue,CornerRadii.EMPTY, Insets.EMPTY)));
+        });
+        label.backgroundProperty().bind(colorPicker.backgroundProperty());
+        label.prefWidthProperty().bind(stackPane.widthProperty());
+        label.prefHeightProperty().bind(stackPane.heightProperty());
+        colorPicker.prefWidthProperty().bind(stackPane.widthProperty());
+        colorPicker.prefHeightProperty().bind(stackPane.heightProperty());
 
     }
 }
