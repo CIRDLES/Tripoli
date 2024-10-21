7cd026badcd8536b4a775924817934c3bd008e4c
Dan Johnson <jdjohnson8883@gmail.com>
2024-10-21 11:37:04 -0400

    Experimenting with gridpane

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/settings/color/fxcomponents/SpeciesIntensityColorSelectionScrollPane.java
+++ TripoliApp/src/main/java/org/cirdles/tripoli/gui/settings/color/fxcomponents/SpeciesIntensityColorSelectionScrollPane.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class SpeciesIntensityColorSelectionScrollPane extends ScrollPane {
 
@@ -27,17 +29,38 @@ public class SpeciesIntensityColorSelectionScrollPane extends ScrollPane {
 
     // Method to add column headers
     private void addHeaders() {
-        gridPane.add(new Label("Species"), 0, 0);
-        gridPane.add(new Label("Faraday Data"), 1, 0);
-        gridPane.add(new Label("PM Data"), 2, 0);
-        gridPane.add(new Label("Faraday Model"), 3, 0);
-        gridPane.add(new Label("PM Model"), 4, 0);
+        // Create a monospaced, bold font with size 18
+        String headerStyle = "-fx-font-family: 'Consolas'; -fx-font-weight: bold; -fx-font-size: 14;";
+
+        // Create header labels with the style
+        Label speciesHeader = new Label("Species");
+        speciesHeader.setStyle(headerStyle);
+
+        Label faradayDataHeader = new Label("Faraday Data");
+        faradayDataHeader.setStyle(headerStyle);
+
+        Label pmDataHeader = new Label("PM Data");
+        pmDataHeader.setStyle(headerStyle);
+
+        Label faradayModelHeader = new Label("Faraday Model");
+        faradayModelHeader.setStyle(headerStyle);
+
+        Label pmModelHeader = new Label("PM Model");
+        pmModelHeader.setStyle(headerStyle);
+
+        // Add headers to the grid pane
+        gridPane.add(speciesHeader, 0, 0);
+        gridPane.add(faradayDataHeader, 1, 0);
+        gridPane.add(pmDataHeader, 2, 0);
+        gridPane.add(faradayModelHeader, 3, 0);
+        gridPane.add(pmModelHeader, 4, 0);
 
         // Ensure consistent column widths
         ColumnConstraints col1 = new ColumnConstraints();
         col1.setPercentWidth(20); // Adjust width percentage as needed
         gridPane.getColumnConstraints().addAll(col1, col1, col1, col1, col1);
     }
+
 
     // Method to add a species row using ColorPickerSplotch
     private void addSpeciesRow(String species, ColorPickerSplotch faradayData, ColorPickerSplotch pmData, ColorPickerSplotch faradayModel, ColorPickerSplotch pmModel) {
