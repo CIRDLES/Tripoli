51d95a4984c100cbd7b3f9aecf61a01b56461b8c
Dan Johnson <jdjohnson8883@gmail.com>
2024-12-02 10:21:09 -0500

    Removed old/unused files

--- TripoliApp/src/main/java/org/cirdles/tripoli/gui/settings/color/old/SpeciesColorRowSelectionRecord.java
+++
@@ -1,8 +0,0 @@
-package org.cirdles.tripoli.gui.settings.color.old;
-
-import org.cirdles.tripoli.species.SpeciesColorSetting;
-
-public record SpeciesColorRowSelectionRecord(SpeciesColorPane speciesColorPane,
-                                             SpeciesColorRow speciesColorRow,
-                                             SpeciesColorSetting speciesColorSetting) {
-}
