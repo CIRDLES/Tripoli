f5aa6c6acd8ecca6ce37a90bc97a33fd1e725a84
JalenEvans <jalenemmanuelevans@gmail.com>
2025-07-18 13:13:46 -0400

    Created dataFiles resources

--- TripoliCore/src/test/java/org/cirdles/tripoli/reports/ReportTest.java
+++ TripoliCore/src/test/java/org/cirdles/tripoli/reports/ReportTest.java
@@ -77,75 +77,11 @@ public class ReportTest {
         assertAll(
                 () -> assertTrue(testColumn.equals(dupeColumn), "ReportColumns should be equal"),
                 () -> assertTrue(testCategory.equals(dupeCategory), "ReportCategory should be equal"),
-                () -> assertTrue(testReport.equals(dupeReport),"Report should be equal"),
+                () -> assertTrue(testReport.equals(dupeReport), "Report should be equal"),
 
                 () -> assertFalse(testReport.equals(alteredReport), "Reports should NOT be equal"),
                 () -> assertFalse(testCategory.equals(alteredCategory), "Categories should NOT be equal"),
                 () -> assertFalse(testColumn.equals(alteredColumn), "Columns should NOT be equal")
         );
     }
-
-    /**
-     * Uses a filepath to generate a test report and then asserts it to a premade Oracle made with the same analysis name
-     */
-    @Test
-    public void accurateReportTest() throws URISyntaxException, JAXBException, IOException {
-        // This is the absolute path of the file that is tested
-        String filepath = "C:/Users/redfl/Desktop/CIRDLES/Test Data/TripoliTestData/IsotopxPhoenixTIMS/KU_IGL/IsolinxVersion2/NBS981 230024a.RAW/NBS981 230024a-145.TIMSDP";
-
-        File dataFile = new File(filepath);
-
-        Session tripoliSession = Session.initializeDefaultSession();
-        AnalysisInterface analysisProposed;
-        AnalysisInterface analysis = null;
-        String analysisName = "";
-        try {
-            analysisProposed = AnalysisInterface.initializeNewAnalysis(0);
-            analysisName = analysisProposed.extractMassSpecDataFromPath(Path.of(dataFile.toURI()));
-
-            if (analysisProposed.getMassSpecExtractedData().getMassSpectrometerContext().compareTo(MassSpectrometerContextEnum.UNKNOWN) != 0) {
-
-                analysisProposed.setAnalysisName(analysisName);
-                analysisProposed.setAnalysisStartTime(analysisProposed.getMassSpecExtractedData().getHeader().analysisStartTime());
-                tripoliSession.getMapOfAnalyses().put(analysisProposed.getAnalysisName(), analysisProposed);
-                analysis = analysisProposed;
-            }
-            else {
-                analysis = null;
-            }
-
-        } catch (IOException | JAXBException | TripoliException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
-
-        }
-
-        assertNotNull(analysis);
-        analysis.getUserFunctions().sort(null);
-        Report fullReport = Report.createFullReport("Full Report", analysis);
-        List<AnalysisInterface> analysisList = List.of(analysis);
-        fullReport.generateCSVFile(analysisList, tripoliSession.getSessionName());
-
-        String actualReport = "";
-        String expectedReport = "Oracle not found for file " + dataFile.getName() + " at: TripoliCore/src/test/resources/org/cirdles/tripoli/core/fullReports";
-
-        try {
-            actualReport = FileUtils.readFileToString(new File(filepath.substring(0, filepath.lastIndexOf('/') + 1) + "New Session-" + analysisName + "-report.csv"), "UTF-8");
-
-            /**
-             * To make an Oracle for an analysis:
-             * 1. Manually create a report with Tripoli.
-             * 2. Name it Oracle-{analysis name}-report.csv
-             * 3. Move it into TripoliCore/src/test/resources/org/cirdles/tripoli/core/fullReports
-             * 4. Replace the values for "Data File Path" and "Created On:" to DATA_FILE_PATH and TIME_CREATED, respectively
-             */
-            expectedReport = FileUtils.readFileToString(new File(Objects.requireNonNull(getClass().getResource("/org/cirdles/tripoli/core/fullReports/Oracle-" + analysisName + "-report.csv")).toURI()), "UTF-8")
-                    .replace("DATA_FILE_PATH", dataFile.toPath().toString())
-                    .replace("TIME_CREATED", fullReport.getTimeCreated());
-        }
-        catch (NullPointerException e) {
-            System.out.println(expectedReport);
-        }
-
-        assertEquals(expectedReport, actualReport);
-    }
-
 }
