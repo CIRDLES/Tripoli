ae4d123130a9ccb653ff3a28cdcb3fcc86f845c3
JalenEvans <jalenemmanuelevans@gmail.com>
2025-07-28 14:17:33 -0400

    Suppressed Analysis Start time in test env

--- TripoliCore/src/main/java/org/cirdles/tripoli/reports/ReportCategory.java
+++ TripoliCore/src/main/java/org/cirdles/tripoli/reports/ReportCategory.java
@@ -91,7 +91,7 @@ public class ReportCategory implements Serializable, Comparable<ReportCategory> 
         columnSet.add(new ReportColumn("Data File Name",i++, "getDataFilePathString"));
         if (!Report.supressContents) columnSet.add(new ReportColumn("Data File Path",i++, "getDataFilePathString"));
         columnSet.add(new ReportColumn("Method Name",i++, "getMethod"));
-        columnSet.add(new ReportColumn("Start Time", i, "getAnalysisStartTime"));
+        if (!Report.supressContents) columnSet.add(new ReportColumn("Start Time", i, "getAnalysisStartTime"));
 
         return new ReportCategory("Analysis Info", columnSet, 0);
     }
