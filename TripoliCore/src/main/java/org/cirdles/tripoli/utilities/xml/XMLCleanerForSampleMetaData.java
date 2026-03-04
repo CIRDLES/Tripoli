33cc69e732e685a43e48a275770cdd78d517354d
bowring <bowring@gmail.com>
2026-03-04 12:50:21 -0500

    added xml cleaning

---
+++ TripoliCore/src/main/java/org/cirdles/tripoli/utilities/xml/XMLCleanerForSampleMetaData.java
@@ -0,0 +1,52 @@
+package org.cirdles.tripoli.utilities.xml;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class XMLCleanerForSampleMetaData {
+    public static File cleanXML(String fileName) throws IOException {
+        File cleanedFile = new File("output_cleaned.xml");
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(fileName));
+            String xmlContent = new String(encoded, java.nio.charset.StandardCharsets.UTF_8);
+            int indexOfSample = xmlContent.indexOf("<sampleName>");
+            xmlContent = "<SampleMetaData>\n" + xmlContent.substring(indexOfSample);
+            xmlContent = xmlContent.replaceAll("\n", "");
+
+            Files.write(Paths.get(cleanedFile.getAbsolutePath()),
+                    xmlContent.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+
+            File xmlFile = cleanedFile;
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+
+            // Normalize the XML structure for cleaner processing
+            doc.getDocumentElement().normalize();
+
+            // Write the modified document back to a file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+
+            StreamResult result = new StreamResult(cleanedFile);
+            transformer.transform(source, result);
+
+            System.out.println("SampleMetaData XML file cleaned successfully.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cleanedFile;
+    }
+
+}
+
